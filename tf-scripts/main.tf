e173cee8c8be02932985a9e6b5a4e5fdc272ba81
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-06-24 15:09:08 +0530

    Update main.tf

--- tf-scripts/main.tf
+++ tf-scripts/main.tf
@@ -20,7 +20,7 @@ resource "google_cloud_run_service" "default" {
       containers {
         image = "${var.docker_image}"
         ports {
-          container_port = 80
+          container_port = var.port
          }  
         env {
           name = "EDITOR_UPSTREAM_RENDER_URL"
